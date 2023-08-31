eb04d35672612cbc2a9a9f93e3c87cf93aa4fe47
Tarun Kumar <143592275+tarunvv@users.noreply.github.com>
2023-08-31 14:39:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def return_df(gender,
 
 
 def base_model():
-    bmodel=jb.load(os.path.join('finalised_nb_model.pkl'))
+    bmodel=jb.load(os.path.join('finalized_nb_model.pkl'))
     return bmodel
 
 st.markdown(heading_style, unsafe_allow_html=True)
